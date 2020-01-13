5e8c5fe7b270c9caf0cad0125958c7f15446d6f6
Romullosilva <romullo.dsilva@gmail.com>
2020-01-13 16:58:51 -0300

    subindo page teste de cadastro utilizando post

---
+++ cadastro/src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import{HomeComponent}from'./home/home.component';
+import{ListagemComponent}from'./listagem/listagem.component';
+import{FormularioComponent}from'./formulario/formulario.component';
+
+
+
+
+
+const routes: Routes = [
+{path: '' , component: HomeComponent},
+{path: 'home' , component: HomeComponent},
+{path: 'listagem' , component: ListagemComponent},
+{path: 'novo' , component: FormularioComponent},
+
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
